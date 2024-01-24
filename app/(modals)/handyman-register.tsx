3f279756988aa4673914f6108b1a6cb22d7d01a9
Murage-Ivy <ivymurage2000@gmail.com>
2024-01-24 17:20:58 +0300

    Update UI for handyman registration page

--- app/(modals)/handyman-register.tsx
+++ app/(modals)/handyman-register.tsx
@@ -1,4 +1,4 @@
-import { View, Text, Image, StyleSheet, TextInput, Pressable, ScrollView } from 'react-native'
+import { View, Text, Image, StyleSheet, TextInput, Pressable, ScrollView, SafeAreaView } from 'react-native'
 import React from 'react'
 import Colors from '@/constants/Colors'
 import { defaultStyles } from '@/constants/styles'
@@ -13,99 +13,84 @@ const HandymanRegister = () => {
         router.push('/(image-picker)/image-picker')
     }
     return (
-        <View style={handymanRegisterStyles.container}>
-            <View style={{ alignItems: 'center' }}>
-                <Image style={{ top: 24, zIndex: 10, width: 260, height: 200 }} source={require('@/assets/images/handyman.png')} />
-            </View>
-            <ScrollView contentContainerStyle={
-                {
-                    flexGrow: 1,
-                    justifyContent: 'space-evenly',
-                    alignItems: 'center',
-                    overflow: 'scroll',
-                    marginTop: 20,
-                    paddingBottom: 20,
-                    backgroundColor: Colors.light
+        <SafeAreaView style={{ flex: 1, paddingTop: 20, backgroundColor: Colors.primary }}>
+            <View style={handymanRegisterStyles.container}>
+                <View style={{ alignItems: 'center' }}>
+                    <Image style={{ top: 28, zIndex: 10, width: 260, height: 200 }} source={require('@/assets/images/handyman.png')} />
+                </View>
+                <ScrollView contentContainerStyle={handymanRegisterStyles.contentContainer} style={handymanRegisterStyles.scroll}>
+                    <View style={handymanRegisterStyles.subContainer}>
 
-                }
+                        <TextInput
+                            autoCapitalize='none'
+                            placeholder='First Name'
+                            style={[defaultStyles.inputTextField, handymanRegisterStyles.textInput]}
+                        />
+                        <TextInput
+                            autoCapitalize='none'
+                            placeholder='Second Name'
+                            style={[defaultStyles.inputTextField, handymanRegisterStyles.textInput]}
+                        />
+                        <TextInput
+                            autoCapitalize='none'
+                            placeholder='Title (e.g Carpenter)'
+                            style={[defaultStyles.inputTextField, handymanRegisterStyles.textInput]}
+                        />
+                        <Select
+                            data={['Service', 'Handyman']}
+                            defaultButtonText='Service'
+                            searchPlaceHolder='Search for a service'
+                        />
 
-            } style={handymanRegisterStyles.scroll}>
+                        <TextInput
+                            autoCapitalize='none'
+                            placeholder='Title (e.g Carpenter)'
+                            style={[defaultStyles.inputTextField, handymanRegisterStyles.textInput]}
+                        />
+                        <TextInput
+                            autoCapitalize='none'
+                            placeholder='Years of  Experience (e.g 1)'
+                            style={[defaultStyles.inputTextField, handymanRegisterStyles.textInput]}
+                        />
+                        <Select
+                            data={['Years', 'Months']}
+                            defaultButtonText='Location'
+                            searchPlaceHolder='Search for a Location'
+                        />
+                        <TextInput
+                            autoCapitalize='none'
+                            multiline
+                            numberOfLines={10}
+                            placeholder='Description(e.g I am a plumber .. '
+                            style={[defaultStyles.inputTextField, handymanRegisterStyles.textInput, handymanRegisterStyles.textArea]}
+                        />
 
+                        <TextInput
+                            autoCapitalize='none'
+                            multiline
+                            numberOfLines={10}
+                            placeholder='Task Responsibilities e.g(cleaning, house arrangement) '
+                            style={[defaultStyles.inputTextField, handymanRegisterStyles.textInput, handymanRegisterStyles.textArea]}
 
+                        />
 
-                <View style={handymanRegisterStyles.subContainer}>
-
-                    <TextInput
-                        autoCapitalize='none'
-                        placeholder='First Name'
-                        style={[defaultStyles.inputTextField, handymanRegisterStyles.textInput]}
-                    />
-                    <TextInput
-                        autoCapitalize='none'
-                        placeholder='Second Name'
-                        style={[defaultStyles.inputTextField, handymanRegisterStyles.textInput]}
-                    />
-                    <TextInput
-                        autoCapitalize='none'
-                        placeholder='Title (e.g Carpenter)'
-                        style={[defaultStyles.inputTextField, handymanRegisterStyles.textInput]}
-                    />
-                    <Select
-                        data={['Service', 'Handyman']}
-                        defaultButtonText='Service'
-                        searchPlaceHolder='Search for a service'
-                    />
-
-                    <TextInput
-                        autoCapitalize='none'
-                        placeholder='Title (e.g Carpenter)'
-                        style={[defaultStyles.inputTextField, handymanRegisterStyles.textInput]}
-                    />
-                    <TextInput
-                        autoCapitalize='none'
-                        placeholder='Years of  Experience (e.g 1)'
-                        style={[defaultStyles.inputTextField, handymanRegisterStyles.textInput]}
-                    />
-                    <Select
-                        data={['Years', 'Months']}
-                        defaultButtonText='Location'
-                        searchPlaceHolder='Search for a Location'
-                    />
-                    <TextInput
-                        autoCapitalize='none'
-                        multiline
-                        numberOfLines={10}
-                        placeholder='Description(e.g I am a plumber .. '
-                        style={[defaultStyles.inputTextField, handymanRegisterStyles.textInput, handymanRegisterStyles.textArea]}
-                    />
-
-                    <TextInput
-                        autoCapitalize='none'
-                        multiline
-                        numberOfLines={10}
-                        placeholder='Task Responsibilities e.g(cleaning, house arrangement) '
-                        style={[defaultStyles.inputTextField, handymanRegisterStyles.textInput, handymanRegisterStyles.textArea]}
-
-                    />
-
-                    <Pressable style={handymanRegisterStyles.submitBtn} onPress={handleSubmit}><Text style={handymanRegisterStyles.submitBtnText}>Submit</Text></Pressable>
+                        <Pressable style={handymanRegisterStyles.submitBtn} onPress={handleSubmit}><Text style={handymanRegisterStyles.submitBtnText}>Submit</Text></Pressable>
 
 
-                </View >
-            </ScrollView >
-        </View >
+                    </View >
+                </ScrollView >
+            </View >
+        </SafeAreaView >
     )
 }
 
 
 const handymanRegisterStyles = StyleSheet.create({
     container: {
-        backgroundColor: Colors.primary,
         flex: 1,
         justifyContent: 'space-evenly',
         alignItems: 'center',
         overflow: 'scroll',
-        marginTop: 20,
     },
     scroll: {
         width: '100%',
@@ -115,11 +100,8 @@ const handymanRegisterStyles = StyleSheet.create({
         backgroundColor: Colors.light,
         justifyContent: 'space-evenly',
         alignItems: 'center',
-        padding: 12,
         overflow: 'scroll',
-        paddingBottom: 30
-
-
+        paddingBottom: 30,
     },
     textInput: {
         borderColor: Colors.secondary,
@@ -146,6 +128,19 @@ const handymanRegisterStyles = StyleSheet.create({
         textAlign: 'center'
 
     },
+    contentContainer: {
+        flexGrow: 1,
+        justifyContent: 'space-evenly',
+        alignItems: 'center',
+        overflow: 'scroll',
+        marginTop: 20,
+        paddingBottom: 20,
+        paddingTop: 25,
+        backgroundColor: Colors.light,
+        borderTopLeftRadius: 16,
+        borderTopRightRadius: 16
+
+    }
 })
 export default HandymanRegister
 
