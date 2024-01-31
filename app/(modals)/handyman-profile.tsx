4d13aefe6bff4bccc4d0e006a5f4967469f12d25
Murage-Ivy <ivymurage2000@gmail.com>
2024-01-31 21:25:33 +0300

    Convert year of experience to string

--- app/(modals)/handyman-profile.tsx
+++ app/(modals)/handyman-profile.tsx
@@ -243,7 +243,7 @@ const HandymanProfile = () => {
                                     <TextInput
                                         autoCapitalize='none'
                                         placeholder='year of  Experience (e.g 1)'
-                                        value={values.year_of_experience!}
+                                        value={values.year_of_experience!.toString()}
                                         onChangeText={handleChange('year_of_experience')}
                                         onBlur={() => setFieldTouched('year_of_experience')}
                                         style={[defaultStyles.inputTextField, HandymanProfileStyles.textInput]}
