d37952d5b982d21bc86bccdaaee6551cd819fe82
Murage-Ivy <ivymurage2000@gmail.com>
2024-02-02 13:29:26 +0300

    Create and style search component

--- components/search.tsx
+++ components/search.tsx
@@ -1,5 +1,6 @@
 import { View, TextInput, StyleSheet } from 'react-native'
 import React from 'react'
+import { FontAwesome5 } from '@expo/vector-icons'
 
 
 type SearchType = {
@@ -14,13 +15,38 @@ const Search: React.FC<SearchType> = (props) => {
             <TextInput
                 autoCapitalize='none'
                 placeholder={placeholder}
-                style={[searchStyles]}
+                style={[searchStyles.container]}
             />
+            <FontAwesome5 name="search" size={20} color="rgba(0, 0, 0, .28)" style={[searchStyles.searchIcon]} />
         </View>
 
     )
 }
 const searchStyles = StyleSheet.create({
 
+    container: {
+        backgroundColor: 'white',
+        fontFamily: 'poppins',
+        fontSize: 14,
+        letterSpacing: 1.2,
+        padding: 10,
+        paddingLeft: 48,
+        borderRadius: 10,
+        margin: 20,
+        width: '90%',
+        shadowColor: "#000",
+        shadowOffset: {
+            width: 0,
+            height: 2,
+        },
+        shadowOpacity: 0.25,
+        shadowRadius: 3.84,
+        elevation: 5,
+    },
+    searchIcon: {
+        position: 'absolute',
+        top: 32,
+        left: 40
+    }
 })
 export default Search
