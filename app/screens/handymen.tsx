e8c1db83640c5536ac15735dd791627cb2587d0c
Murage-Ivy <ivymurage2000@gmail.com>
2024-02-06 19:17:53 +0300

    Create a handyman context

--- app/screens/handymen.tsx
+++ app/screens/handymen.tsx
@@ -5,32 +5,38 @@ import { defaultJobStyles } from '@/constants/styles'
 import { FontAwesome5 } from '@expo/vector-icons'
 import { Image } from 'expo-image'
 import Search from '@/components/search'
+import { HandymanProvider, useHandyman } from '@/context/HandymanContext'
+import Loader from '@/components/loader'
 
 const Handymen = () => {
+    const { loading } = useHandyman()
     const handleChange = () => {
         console.log('searching...')
     }
     return (
-        <View>
-
-            <View style={defaultJobStyles.headerStyle}>
-                <Image source={require('@/assets/images/placeholder.jpg')}
-                    style={{ width: 50, height: 50, borderRadius: 50 }} />
-                <Text>Plumbing</Text>
-                <FontAwesome5
-                    name="bell"
-                    size={20}
-                    color={Colors.secondary}
-                />
-            </View>
-
+        <HandymanProvider>
             <View>
-                <Search handleChange={handleChange} placeholder='Search for Handyman' />
+
+                <View style={defaultJobStyles.headerStyle}>
+                    <Image source={require('@/assets/images/placeholder.jpg')}
+                        style={{ width: 50, height: 50, borderRadius: 50 }} />
+                    <Text>Plumbing</Text>
+                    <FontAwesome5
+                        name="bell"
+                        size={20}
+                        color={Colors.secondary}
+                    />
+                </View>
+
+                <View>
+                    <Search handleChange={handleChange} placeholder='Search for Handyman' />
+                </View>
+                <View>
+                    <Text>Recommended</Text>
+                </View>
             </View>
-            <View>
-                <Text>Recommended</Text>
-            </View>
-        </View>
+            <Loader isLoading={loading!} />
+        </HandymanProvider>
     )
 }
 
--- context/HandymanContext.tsx
+++ context/HandymanContext.tsx
@@ -1,6 +1,9 @@
-import { createContext, useContext } from "react";
+import { getItemAsync } from "expo-secure-store";
+import { createContext, useContext, useEffect, useState } from "react";
+import { useAuth } from "./AuthContext";
 
 interface HandymanProps {
+    loading?: boolean
 
 }
 interface HandymanProviderProps {
@@ -17,8 +20,52 @@ export const useHandyman = () => {
 }
 
 export const HandymanProvider = ({ children }: HandymanProviderProps) => {
+    const { authState } = useAuth()
+    const [loading, setLoading] = useState<boolean>(false)
+
+    useEffect(() => {
+        const getHandymen = async () => {
+            const service_id = await getItemAsync('service_id')
+
+            try {
+                const response = await fetch(`${process.env.EXPO_PUBLIC_API_URL}/handymen?service_id=${service_id}`, {
+                    headers: { Authorization: `Bearer ${authState?.token}` }
+                })
+                const data = await response.json()
+                if (!response.ok) {
+                    let error;
+                    if (data.message) {
+                        error = data.message;
+                    } else if (data.error) {
+                        error = data.error;
+                    } else {
+                        error = response.statusText;
+                    }
+                    throw new Error(error);
+                }
+
+                if (response.ok) {
+                    console.log(data)
+                    setLoading(false)
+                }
+            }
+            catch (err: any) {
+                console.log(err.message)
+                setLoading(false)
+            }
+            finally {
+                setLoading(false)
+            }
+
+        }
+        getHandymen()
+    }, [])
+
+    const value = {
+        loading
+    }
     return (
-        <HandymanContext.Provider value={{}}>
+        <HandymanContext.Provider value={value}>
             {children}
         </HandymanContext.Provider>
     )
