17b8bfc40a86b87db2b4dfe63bfd50eb1e949886
Murage-Ivy <ivymurage2000@gmail.com>
2024-02-15 14:14:08 +0300

    update depencies

--- app/(modals)/appointment-form.tsx
+++ app/(modals)/appointment-form.tsx
@@ -1,11 +1,25 @@
-import { View, Text } from 'react-native'
+import { View, Text, SafeAreaView } from 'react-native'
 import React from 'react'
+import Colors from '@/constants/Colors'
+import { Image } from 'expo-image'
 
 const AppointmentForm = () => {
     return (
-        <View>
-            <Text>AppointmentForm</Text>
-        </View>
+        <SafeAreaView style={{
+            flex: 1,
+            paddingTop: 20,
+            backgroundColor: Colors.primary
+        }}>
+            <View>
+                <Image source={require('@/assets/images/calendar.svg')} style={{ width: 200, height: 200 }} />
+                <Text>Book an Appointment</Text>
+
+                <View>
+
+                </View>
+            </View>
+        </SafeAreaView>
+
     )
 }
 
