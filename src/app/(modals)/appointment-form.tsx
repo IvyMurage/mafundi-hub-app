b30643e705dfe067f841fc4cbf9e312441f90f89
Murage-Ivy <ivymurage2000@gmail.com>
2024-02-17 18:07:29 +0300

    Create an appointment form

--- src/app/(modals)/appointment-form.tsx
+++ src/app/(modals)/appointment-form.tsx
@@ -1,4 +1,4 @@
-import { View, Text, SafeAreaView, ScrollView, TextInput, Pressable } from 'react-native'
+import { View, Text, SafeAreaView, ScrollView, TextInput, Pressable, StyleSheet } from 'react-native'
 import React from 'react'
 import Colors from '@/constants/Colors'
 import { Image } from 'expo-image'
@@ -10,54 +10,28 @@ import { useRouter } from 'expo-router'
 const AppointmentForm = () => {
     const router = useRouter()
     return (
-        <SafeAreaView style={{ flex: 1, paddingTop: 20, backgroundColor: Colors.primary }}>
+        <SafeAreaView style={appointmentStyles.container}>
             <FontAwesome5
                 name='arrow-left'
                 size={24} color={Colors.lighter}
                 onPress={() => { router.back() }}
-                style={{ paddingLeft: 20, marginVertical: 10 }} />
+                style={appointmentStyles.icon} />
+
             <View style={{ justifyContent: 'center', alignItems: 'center' }}>
-                <Image source={require('@/assets/images/calendar.svg')} style={{ width: 200, height: 200 }} contentFit='contain' />
-                <Text
-                    style={{
-                        fontSize: 16,
-                        color: Colors.lighter,
-                        fontFamily: 'roboto-medium',
-                        fontWeight: 'bold',
-                        letterSpacing: 1.2,
-                        alignSelf: 'flex-start',
-                        paddingHorizontal: 20,
-                        marginVertical: 10
-                    }}>
+                <Image
+                    source={require('@/assets/images/calendar.svg')}
+                    style={appointmentStyles.image}
+                    contentFit='contain' />
+
+                <Text style={appointmentStyles.title}>
                     Book an Appointment
                 </Text>
-                <ScrollView style={{
-                    width: '100%'
-                }} contentContainerStyle={{
-                    justifyContent: 'center',
-                    alignItems: 'center',
-                    backgroundColor: Colors.light,
-                    borderTopLeftRadius: 20,
-                    borderTopRightRadius: 20,
-                    paddingBottom: 20
-                }} showsVerticalScrollIndicator={false}
-                >
-                    <View style={{
-                        backgroundColor: Colors.lighter,
-                        padding: 20,
-                        borderTopLeftRadius: 20,
-                        borderTopRightRadius: 20,
-                        width: '100%',
-                        shadowColor: '#000',
-                        shadowOffset: {
-                            width: 0,
-                            height: 2
-                        },
-                        shadowOpacity: 0.25,
-                        shadowRadius: 3.84,
-                        elevation: 5
 
-                    }}>
+                <ScrollView
+                    style={appointmentStyles.scrollView}
+                    contentContainerStyle={appointmentStyles.scrollViewContent}
+                    showsVerticalScrollIndicator={false}>
+                    <View style={appointmentStyles.calendarContainer}>
                         <CalendarPicker
                             onDateChange={(date) => { console.log(date) }}
                             width={350}
@@ -81,123 +55,45 @@ const AppointmentForm = () => {
                             }}
                         />
                     </View>
-                    <View style={{ marginBottom: 100, width: '100%', justifyContent: 'space-between' }}>
-                        <View style={{
-                            flexDirection: 'row',
-                            justifyContent: 'space-between',
-                            width: '100%',
-                            paddingHorizontal: 20
-                        }}>
+
+                    <View style={appointmentStyles.textContainer}>
+                        <View style={appointmentStyles.inputContainter}>
                             <TextInput
                                 placeholder='Task duration'
-                                style={{
-                                    borderBottomWidth: 1,
-                                    borderBottomColor: Colors.primary,
-                                    marginVertical: 10,
-                                    padding: 10,
-                                    width: '40%',
-                                    letterSpacing: 1.2
-                                }}
+                                style={appointmentStyles.textInput}
                             />
 
                             <TextInput
                                 placeholder='Task Time'
-                                style={{
-                                    borderBottomWidth: 1,
-                                    borderBottomColor: Colors.primary,
-                                    marginVertical: 10,
-                                    padding: 10,
-                                    width: '40%',
-                                    letterSpacing: 1.2
-                                }}
+                                style={appointmentStyles.textInput}
                             />
                         </View>
 
-                        <View style={{
-                            flexDirection: 'row',
-                            justifyContent: 'space-between',
-                            width: '100%',
-                            paddingHorizontal: 20
-                        }}>
+                        <View style={appointmentStyles.inputContainter}>
                             <TextInput
-                                placeholder='Enter Phone number(07xxxx)'
-                                style={{
-                                    borderBottomWidth: 1,
-                                    borderBottomColor: Colors.primary,
-                                    marginVertical: 10,
-                                    padding: 10,
-                                    width: '40%',
-                                    letterSpacing: 1.2
-                                }}
+                                placeholder='Phone number(07xx)'
+                                style={appointmentStyles.textInput}
                             />
                             <TextInput
                                 placeholder='Enter Amount'
-                                style={{
-                                    borderBottomWidth: 1,
-                                    borderBottomColor: Colors.primary,
-                                    marginVertical: 10,
-                                    padding: 10,
-                                    width: '40%',
-                                    letterSpacing: 1.2
-                                }}
+                                style={appointmentStyles.textInput}
                             />
                         </View>
 
                         <TextInput
                             placeholder='Appointment Notes'
-                            style={{
-                                borderBottomWidth: 1,
-                                borderBottomColor: Colors.primary,
-                                // marginVertical: 10,
-                                paddingHorizontal: 10,
-                                marginHorizontal: 20,
-                                letterSpacing: 1.2
-                            }}
+                            style={appointmentStyles.textArea}
                             multiline
                             numberOfLines={6}
                         />
-
-                        <View style={{
-                            paddingHorizontal: 20,
-                            marginVertical: 10,
-                            width: '100%',
-                            justifyContent: 'space-between'
-
-                        }}>
-
-
-                            <Pressable style={{
-                                backgroundColor: Colors.secondary,
-                                padding: 20,
-                                borderRadius: 5,
-                                justifyContent: 'center',
-                                alignItems: 'center',
-                                marginVertical: 10
-                            }}>
-                                <Text style={{
-                                    color: Colors.lighter,
-                                    fontFamily: 'roboto-medium',
-                                    fontWeight: 'bold',
-                                    letterSpacing: 1.2
-                                }}>
+                        <View style={appointmentStyles.buttonContainer}>
+                            <Pressable style={appointmentStyles.button}>
+                                <Text style={appointmentStyles.buttonTitle}>
                                     Lipa na Mpesa
                                 </Text>
                             </Pressable>
-                            <Pressable style={{
-                                backgroundColor: Colors.primary,
-                                padding: 20,
-                                borderRadius: 5,
-                                justifyContent: 'center',
-                                alignItems: 'center',
-                                marginVertical: 10
-
-                            }}>
-                                <Text style={{
-                                    color: Colors.lighter,
-                                    fontFamily: 'roboto-medium',
-                                    fontWeight: 'bold',
-                                    letterSpacing: 1.2
-                                }}>
+                            <Pressable style={[appointmentStyles.button, { backgroundColor: Colors.primary }]}>
+                                <Text style={[appointmentStyles.buttonTitle,]}>
                                     Confirm and Chat
                                 </Text>
                             </Pressable>
@@ -207,7 +103,105 @@ const AppointmentForm = () => {
             </View>
         </SafeAreaView>
 
-    )
+    );
 }
+const appointmentStyles = StyleSheet.create({
+    container: {
+        flex: 1,
+        paddingTop: 20,
+        backgroundColor: Colors.primary
+    },
+    icon: {
+        paddingLeft: 20,
+        marginVertical: 10
+    },
+    image: {
+        width: 200,
+        height: 200
+    },
+    title: {
+        fontSize: 16,
+        color: Colors.lighter,
+        fontFamily: 'roboto-medium',
+        fontWeight: 'bold',
+        letterSpacing: 1.2,
+        alignSelf: 'flex-start',
+        paddingHorizontal: 20,
+        marginVertical: 10
+    },
+    scrollView: {
+        width: '100%'
+    },
+    scrollViewContent: {
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: Colors.light,
+        borderTopLeftRadius: 20,
+        borderTopRightRadius: 20,
+        paddingBottom: 20,
+    },
+    calendarContainer: {
+        backgroundColor: Colors.lighter,
+        padding: 20,
+        borderTopLeftRadius: 20,
+        borderTopRightRadius: 20,
+        width: '100%',
+        shadowColor: '#000',
+        shadowOffset: {
+            width: 0,
+            height: 2
+        },
+        shadowOpacity: 0.25,
+        shadowRadius: 3.84,
+        elevation: 5
+    },
+    textInput: {
+        borderBottomWidth: 1,
+        borderBottomColor: Colors.primary,
+        marginVertical: 10,
+        padding: 10,
+        width: '45%',
+        letterSpacing: 1.2
+    },
+    button: {
+        backgroundColor: Colors.secondary,
+        padding: 20,
+        borderRadius: 5,
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginVertical: 10
+    },
+    buttonTitle: {
+        color: Colors.lighter,
+        fontFamily: 'roboto-medium',
+        fontWeight: 'bold',
+        letterSpacing: 1.2
+    },
+    buttonContainer: {
+        paddingHorizontal: 20,
+        marginVertical: 10,
+        width: '100%',
+        justifyContent: 'space-between'
+    },
+    textArea: {
+        borderBottomWidth: 1,
+        borderBottomColor: Colors.primary,
+        marginVertical: 10,
+        paddingHorizontal: 10,
+        marginHorizontal: 20,
+        letterSpacing: 1.2
+    },
+    inputContainter: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        width: '100%',
+        paddingHorizontal: 20
+    },
+    textContainer: {
+        marginBottom: 150,
+        width: '100%',
+        justifyContent: 'space-between'
+    }
 
+})
 export default AppointmentForm
