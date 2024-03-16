c757b638ee3d39bc825e6f076bc16075e651b0cb
Murage-Ivy <ivymurage2000@gmail.com>
2024-03-16 15:44:29 +0300

    Create an update and delete appointment functionality

--- src/app/(screens)/notifications.tsx
+++ src/app/(screens)/notifications.tsx
@@ -6,6 +6,7 @@ import { Ionicons } from '@expo/vector-icons';
 import Loader from '@/components/loader';
 import CustomAlert from '@/components/customAlert';
 import { Menu, MenuOption, MenuOptions, MenuTrigger } from 'react-native-popup-menu';
+import { request } from '@/utils/executePostRequest';
 
 type AppointmentType = {
     id: number;
@@ -14,10 +15,11 @@ type AppointmentType = {
     appointment_status: string;
     handyman_name: string;
     handyman_id: number;
+    job_proposal_id: number;
 }
 const Appointments = () => {
     const [loading, setLoading] = useState(true)
-    const [appointments, setAppointments] = useState([])
+    const [appointments, setAppointments] = useState<AppointmentType[]>([])
     const { userState, authState } = useAuth()
     const [error, setError] = useState('')
     const [visible, setVisible] = useState(false)
@@ -34,12 +36,13 @@ const Appointments = () => {
                 if (response.ok) {
                     setAppointments(data?.appointment?.map((appointment: AppointmentType) => {
                         return {
-                            appointment_id: appointment.id,
+                            id: appointment.id,
                             appointment_date: appointment.appointment_date,
                             task_name: appointment.task_name,
                             appointment_status: appointment.appointment_status,
                             handyman_name: appointment.handyman_name,
-                            handyman_id: appointment.handyman_id
+                            handyman_id: appointment.handyman_id,
+                            job_proposal_id: appointment.job_proposal_id
                         }
                     }))
                 }
@@ -77,6 +80,7 @@ const Appointments = () => {
                     Authorization: `Bearer ${authState?.token}`
                 }
             })
+            setAppointments(prevState => prevState.filter(appointment => appointment.id !== id))
         }
         catch (error) {
             console.error(error)
@@ -85,19 +89,91 @@ const Appointments = () => {
             setLoading(false)
         }
     }
-    const handleUpdate = (id: number) => { }
+    const handleUpdate = async (item: AppointmentType, status: string) => {
+        console.log('item:', item)
+        setLoading(true)
+        try {
+            const { response, data } = await request('PATCH', JSON.stringify({ appointment_status: status }), `appointments/${item.id}/update`, authState?.token!)
+
+            if (response.ok) {
+                setAppointments(prevState => prevState.map(appointment => {
+                    if (appointment.id === data.id) { return data }
+                    return appointment
+                }))
+            }
+            if (!response.ok) {
+                throw new Error(data.error)
+            }
+        }
+        catch (error) {
+            console.log(error)
+        }
+        finally {
+            setLoading(false)
+        }
+    }
     const renderAppointments = (item: AppointmentType) => {
+        const textColor = item?.appointment_status === 'scheduled' ? Colors.lighter : item.appointment_status === 'confirmed' ? Colors.lighter : null
+        const backgroundColor = item?.appointment_status === 'scheduled' ? Colors.primary : item.appointment_status === 'confirmed' ? Colors.secondary : Colors.lighter
+        const scheduled = item?.appointment_status === 'scheduled' ? Colors.secondary : item.appointment_status === 'confirmed' ? Colors.primary : 'red'
+        const border = item?.appointment_status === 'scheduled' ? Colors.secondary : item.appointment_status === 'confirmed' ? Colors.primary : null
         return (
-            <View style={styles.appointmentView}>
-                <Ionicons name="calendar-number-outline" size={24} color="black" style={{ paddingRight: 12 }} />
-                <View style={{}}>
-                    <Text style={{ fontFamily: 'roboto-bold', letterSpacing: 1.2 }}>{item.task_name}</Text>
-                    <Text style={{ fontFamily: 'roboto', letterSpacing: 1.2 }}>{item.handyman_name}</Text>
-                    <Text style={{ fontFamily: 'roboto', letterSpacing: 1.2 }}>{item.appointment_date}</Text>
-                    <Text style={{ fontFamily: 'roboto', letterSpacing: 1.2, color: Colors.secondary }}>{item.appointment_status}</Text>
+            <View style={[styles.appointmentView, { backgroundColor: backgroundColor!, borderColor: border! }]}>
+                <Ionicons name="calendar-number-outline" size={24} color={textColor!} style={{ paddingRight: 12 }} />
+                <View >
+                    <Text style={[{ fontFamily: 'roboto-bold', letterSpacing: 1.2, color: textColor! }]}>{item.task_name}</Text>
+                    <Text style={[styles.fontStyle, { color: textColor! }]}>{item.handyman_name}</Text>
+                    <Text style={[styles.fontStyle, { color: textColor! }]}>{item.appointment_date}</Text>
+                    <Text style={[styles.fontStyle, { color: scheduled }]}>{item.appointment_status}</Text>
                 </View>
-                <Ionicons name="pencil" size={20} color={Colors.secondary} style={{ alignSelf: 'flex-start' }} onPress={() => handleUpdate(item.id)} />
-                <Ionicons name="trash" size={20} color={Colors.secondary} style={{ alignSelf: 'flex-start' }} onPress={() => handleDelete(item.id)} />
+
+                <Menu style={{ alignSelf: 'flex-start' }}>
+                    <MenuTrigger
+                        style={{
+                            padding: 5,
+                            paddingHorizontal: 10,
+                        }}>
+                        <Ionicons name='chevron-forward' size={20} color={item.appointment_status === 'canceled' ? Colors.secondary : Colors.lighter} />
+                    </MenuTrigger>
+                    <MenuOptions customStyles={
+                        {
+                            optionsContainer: {
+                                backgroundColor: 'white',
+                                padding: 5,
+                                borderRadius: 5,
+                                width: 100,
+                            }
+                        }
+                    }>
+                        <MenuOption style={{ width: 100, }} onSelect={() => {
+                            handleUpdate(item, 'confirmed')
+                        }}>
+                            <Text style={{
+                                padding: 5,
+                                paddingHorizontal: 10,
+                                fontFamily: 'roboto-bold'
+                            }} >Confirmed</Text>
+                        </MenuOption>
+                        <MenuOption style={{ width: 100, }} onSelect={() => {
+                            handleUpdate(item, 'canceled')
+                        }}>
+                            <Text style={{
+                                padding: 5,
+                                paddingHorizontal: 10,
+                                fontFamily: 'roboto-bold'
+                            }} >Canceled</Text>
+                        </MenuOption>
+                        <MenuOption style={{ width: 100, }} onSelect={() => {
+                            handleDelete(item.id)
+                        }}>
+                            <Text style={{
+                                padding: 5,
+                                paddingHorizontal: 10,
+                                fontFamily: 'roboto-bold'
+                            }} >Delete</Text>
+                        </MenuOption>
+                    </MenuOptions>
+                </Menu>
             </View>
         )
     }
@@ -130,11 +206,16 @@ const styles = StyleSheet.create({
         flexDirection: 'row',
         alignItems: 'center',
         justifyContent: 'space-between',
-        borderLeftWidth: 4,
+        borderLeftWidth: 5,
         maxHeight: 200,
         borderLeftColor: Colors.secondary,
         marginBottom: 10,
-        paddingHorizontal: 12
+        paddingHorizontal: 12,
+        borderRadius: 10,
+        paddingVertical: 10
+    },
+    fontStyle: {
+        fontFamily: 'roboto', letterSpacing: 1.2
     },
     header: {
         fontSize: 20,
